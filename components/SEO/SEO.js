e489a5c6bc673fc556c41951b2a51e72b87c9cb6
Huy Nguyen <quanghuy871@gmail.com>
2022-04-29 12:10:36 +0700

    Updated

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Head from 'next/head';
-// import settings from '../../settings';
 
 const SEO = (props) => {
   const {title, description, image} = props;
@@ -10,27 +8,13 @@ const SEO = (props) => {
     <Head>
       <title>{title}</title>
       <meta name="description" content={description}/>
+      <meta property="og:url" content="https://nft-seo.vercel.app/"/>
+      <meta property="og:site_name" content="pixl.page"/>
       <meta property="og:name" content={title}/>
       <meta property="og:description" content={description}/>
       <meta property="og:image" content={image.src}/>
     </Head>
   );
 };
-
-// SEO.defaultProps = {
-//   title: settings && settings.meta && settings.meta.title,
-//   description: settings && settings.meta && settings.meta.description,
-//   image:
-//     settings &&
-//     settings.meta &&
-//     settings.meta.social &&
-//     settings.meta.social.graphic,
-// };
-//
-// SEO.propTypes = {
-//   title: PropTypes.string,
-//   description: PropTypes.string,
-//   image: PropTypes.string,
-// };
 
 export default SEO;
