fac2eff2353fda274918c3f6a854e4647cfd49c8
Huy Nguyen <quanghuy871@gmail.com>
2022-05-06 09:40:54 +0700

    updated

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -18,8 +18,8 @@ const SEO = (props) => {
         props.type && <meta property="og:image:type" content="image/jpeg"/>
       }
 
-      <meta property="og:image:width" content= "480" />
-      <meta property="og:image:height" content= "480" />
+      {/*<meta property="og:image:width" content= "480" />*/}
+      {/*<meta property="og:image:height" content= "480" />*/}
 
       <meta property="twitter:title" content={title}/>
       <meta property="twitter:site_name" content="pixl.page"/>
